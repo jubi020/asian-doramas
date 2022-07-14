23a7bead84dc57c070173bb0dc3475d4885a8fe1
jubi020 <jubikumari020@gmail.com>
2022-07-14 11:53:11 +0530

    null

--- client/src/components/views/login/login.js
+++ client/src/components/views/login/login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Form} from 'react-bootstrap';
 import "./login.css";
 import Footer from "../../footer/footer";
+import NavBar from "../../navbar/navbar";
 import {loginUser} from "../../../actions/user_actions";
 import * as Yup from 'yup';
 import {Formik} from 'formik';
@@ -60,43 +61,44 @@ function Login(props) {
                     const {values, touched, errors, dirty, isSubmitting, handleChange, handleBlur, handleSubmit, handleReset,} = props;
                     return (
                         <>
-                        <div className="container">
-                            <div className='login w-100' style={{ maxWidth: "350px" }}>
-                                <h1 className='login-heading'>Log In</h1>
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group id='email' className='form'>
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type='email'
-                                        className={`form-el ${errors.email && touched.email ? 'text-input error' : 'text-input'} `}
-                                        name='email'
-                                        value={values.email}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        required />
-                                        {errors.email && touched.email && (<div className='input-feedback'>{errors.email}</div>)}
-                                    </Form.Group>
-                                    <br />
-                                    <Form.Group id='password' className='form'>
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type='password'
-                                        className={`form-el ${errors.password && touched.password ? 'text-input error':'text-input'}`}
-                                        name='password'
-                                        value={values.password}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        required />
-                                        {errors.password && touched.password && <div className='input-feedback'>{errors.password}</div>}
-                                    </Form.Group>
-                                    <br />
-                                    <button className='w-100 signup--button' type='submit'>Login</button>
+                            <NavBar />
+                            <div className="container">
+                                <div className='login w-100' style={{ maxWidth: "350px" }}>
+                                    <h1 className='login-heading'>Log In</h1>
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Group id='email' className='form'>
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type='email'
+                                            className={`form-el ${errors.email && touched.email ? 'text-input error' : 'text-input'} `}
+                                            name='email'
+                                            value={values.email}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            required />
+                                            {errors.email && touched.email && (<div className='input-feedback'>{errors.email}</div>)}
+                                        </Form.Group>
+                                        <br />
+                                        <Form.Group id='password' className='form'>
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control type='password'
+                                            className={`form-el ${errors.password && touched.password ? 'text-input error':'text-input'}`}
+                                            name='password'
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            required />
+                                            {errors.password && touched.password && <div className='input-feedback'>{errors.password}</div>}
+                                        </Form.Group>
+                                        <br />
+                                        <button className='w-100 signup--button' type='submit'>Login</button>
 
-                                    <div className='no_account_div'>
-                                      <p>Don't have an account! <a href="/register">Signup</a></p>
-                                    </div>
-                                </Form>
+                                        <div className='no_account_div'>
+                                        <p>Don't have an account! <a href="/register">Signup</a></p>
+                                        </div>
+                                    </Form>
+                                </div>
                             </div>
-                        </div>
-                        <Footer />
+                            <Footer />
                         </>
                     )
                 }
