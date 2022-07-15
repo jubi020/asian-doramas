7710ff7e2352f88b29fca343342e2479efde50cb
jubik02 <65113597+jubi020@users.noreply.github.com>
2022-07-15 19:46:25 +0530

    Update config.js

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -2,7 +2,9 @@ export const API_KEY = `${process.env.REACT_APP_TMDB_API_KEY}`;
 
 export const API_URL = 'https://api.themoviedb.org/3/tv';
 
-export const DISCOVER_API_URL = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&include_null_first_air_dates=false`;
+export const DISCOVER_API_URL = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&include_null_first_air_dates=false`
 
 export const baseImageURL = 'https://image.tmdb.org/t/p';
+
+
 
