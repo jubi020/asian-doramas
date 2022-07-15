e02cb8f35532567aa8c79f3cf5fc35b44c7ca905
jubik02 <65113597+jubi020@users.noreply.github.com>
2022-07-15 19:44:42 +0530

    Update config.js

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -6,17 +6,3 @@ export const DISCOVER_API_URL = `https://api.themoviedb.org/3/discover/tv?api_ke
 
 export const baseImageURL = 'https://image.tmdb.org/t/p';
 
-// export const dramaDetailsEndpoint = 'https://api.themoviedb.org/3/tv/133091?api_key=7e10aae68988f9a53b490ed984e5c2e2&language=en-US';
-//ja, ko, zh, cn
-
-
-/* correct one for fetching asian dramas
-https://api.themoviedb.org/3/discover/tv?api_key=7e10aae68988f9a53b490ed984e5c2e2&include_null_first_air_dates=false&
-language=en-US&with_original_language=ko&page=1
-*/
-
-
-/* trial one 
-https://api.themoviedb.org/3/discover/tv?api_key=7e10aae68988f9a53b490ed984e5c2e2&
-language=en-US&sort_by=popularity.desc&page=1&include_null_first_air_dates=false&with_original_language=ko
-*/
